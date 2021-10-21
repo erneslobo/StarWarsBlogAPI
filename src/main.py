38d4a6f7990ecb9b5fbd76f60bae73f9e57c1018
erneslobo <erneslobo@gmail.com>
2021-10-21 02:05:07 +0000

    models completed

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Character, Planet
+from models import db, User, Character, Planet, FavoritePlanet, FavoriteCharacter
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,17 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+@app.route('/users', methods=['GET'])
+def get_users():
+    user_query = User.query.all()
+    print(user_query)
+    all_users = list(map(lambda x: x.serialize(), user_query))
+    print(all_users)
+    return jsonify(all_users)
+    # map the results and your list of people  inside of the all_people variable
+    
+    # response_body = user_query
+    # return jsonify(response_body), 200
 
 @app.route('/people', methods=['GET'])
 def get_people():
@@ -45,7 +48,7 @@ def get_people():
     # map the results and your list of people  inside of the all_people variable
     all_people = list(map(lambda x: x.serialize(), people_query))
     response_body = all_people
-    return jsonify(response_body), 200
+    return jsonify(all_people), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -16,24 +16,26 @@ class Character(db.Model):
     height = db.Column(db.Integer, nullable=False)
     mass = db.Column(db.String(250), nullable=False)
     hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
     eye_color = db.Column(db.String(250), nullable=False)
     birth_year = db.Column(db.String(250), nullable=False)
     gender = db.Column(db.Enum(Gender), nullable=False)
     created = db.Column(db.DateTime, nullable=False)
     edited = db.Column(db.DateTime, nullable=False)
     homeworld = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    favorite_character = db.relationship('FavoriteCharacter', backref='character', lazy=True)
 
     def __repr__(self):
         return f'<Character {self.name}>'
 
     def serialize(self):
-        print(json.dumps(self.gender))
         return {
             "id": self.id,
             "name": self.name,
             "height": self.height,
             "mass": self.mass,
             "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
             "eye_color": self.eye_color,
             "birth_year": self.birth_year,
             "gender": self.gender,
@@ -58,6 +60,7 @@ class Planet(db.Model):
     created= db.Column(db.DateTime, nullable=False)
     edited = db.Column(db.DateTime, nullable=False)
     character = db.relationship('Character', backref='planet', lazy=True)
+    favorite_planet = db.relationship('FavoritePlanet', backref='planet', lazy=True)
 
     def __repr__(self):
         return f'<Planet {self.name}>'
@@ -80,18 +83,53 @@ class Planet(db.Model):
             # do not serialize the password, its a security breach
         }
 
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<FavoriteCharacter {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_id": self.character_id,
+            "user_id": self.user_id,
+            # do not serialize the password, its a security breach
+        }
+
+class FavoritePlanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<FavoritePlanet {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id": self.planet_id,
+            "user_id": self.user_id,
+            # do not serialize the password, its a security breach
+        }
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(250), nullable=False)
+    favorite_character = db.relationship('FavoriteCharacter', backref='user', lazy=True)
+    favorite_planet = db.relationship('FavoritePlanet', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.name}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
